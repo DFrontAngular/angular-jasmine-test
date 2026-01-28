0ee7778fe11f878ef324b93de8764a90efb1a11d
Jose Francisco Ruso Alvarez <jrusoalv@emeal.nttdata.com>
2026-01-28 12:42:26 +0100

    chore: delete ssr and update dependencies

--- angular-jasmine-test-proyect/src/app/app.config.ts
+++ angular-jasmine-test-proyect/src/app/app.config.ts
@@ -1,16 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { appRoutes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptorsFromDi(),withFetch()),
-    
-  ]
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+  ],
 };
