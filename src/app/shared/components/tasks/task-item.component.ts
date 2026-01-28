42d19d9a907d31cc02a5b51d927424e972dd2786
Jose Francisco Ruso Alvarez <jrusoalv@emeal.nttdata.com>
2026-01-28 14:00:38 +0100

    chore: add linter

--- src/app/shared/components/tasks/task-item.component.ts
+++ src/app/shared/components/tasks/task-item.component.ts
@@ -1,10 +1,4 @@
-import {
-    ChangeDetectionStrategy,
-    Component,
-    EventEmitter,
-    Input,
-    Output
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 export interface Task {
   id: number;
@@ -16,9 +10,11 @@ export interface Task {
   selector: 'app-task-item',
   standalone: true,
   templateUrl: './task-item.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskItemComponent {
   @Input({ required: true }) task!: Task;
   @Output() toggle = new EventEmitter<number>();
+
+  test() {}
 }
